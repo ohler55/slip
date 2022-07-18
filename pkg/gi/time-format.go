000ec819b2de3efacab9b8306556f72eec1bd659
Peter Ohler <peter@ohler.com>
2022-07-18 19:32:19 -0400

    lost files

---
+++ pkg/gi/time-format.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2022, Peter Ohler, All rights reserved.
+
+package gi
+
+import (
+	"time"
+
+	"github.com/ohler55/slip"
+)
+
+func init() {
+	slip.Define(
+		func(args slip.List) slip.Object {
+			f := TimeFormat{Function: slip.Function{Name: "time-format", Args: args}}
+			f.Self = &f
+			return &f
+		},
+		&slip.FuncDoc{
+			Name: "time-format",
+			Args: []*slip.DocArg{
+				{
+					Name: "time",
+					Type: "time",
+					Text: "Time to change the location of.",
+				},
+				{
+					Name: "layout",
+					Type: "string",
+					Text: "Layout of the formatted time.",
+				},
+			},
+			Return: "string",
+			Text:   `__time-format__ returns a string representation of the time.`,
+			Examples: []string{
+				`(time-format @2022-07-10T17:29:21Z "2006-01-02") => "2022-07-17"`,
+			},
+		}, &GiPkg)
+}
+
+// TimeFormat represents the timeFormat function.
+type TimeFormat struct {
+	slip.Function
+}
+
+// Call the function with the arguments provided.
+func (f *TimeFormat) Call(s *slip.Scope, args slip.List, depth int) slip.Object {
+	if len(args) != 2 {
+		slip.PanicArgCount(f, 2, 2)
+	}
+	t, ok := args[1].(slip.Time)
+	if !ok {
+		slip.PanicType("time", args[1], "time")
+	}
+	var layout slip.String
+	if layout, ok = args[0].(slip.String); !ok {
+		slip.PanicType("layout", args[0], "string")
+	}
+	return slip.String(time.Time(t).Format(string(layout)))
+}
---
+++ pkg/gi/timep.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2022, Peter Ohler, All rights reserved.
+
+package gi
+
+import (
+	"github.com/ohler55/slip"
+)
+
+func init() {
+	slip.Define(
+		func(args slip.List) slip.Object {
+			f := Timep{Function: slip.Function{Name: "timep", Args: args}}
+			f.Self = &f
+			return &f
+		},
+		&slip.FuncDoc{
+			Name: "timep",
+			Args: []*slip.DocArg{
+				{
+					Name: "object",
+					Type: "object",
+					Text: "Object to check.",
+				},
+			},
+			Text: `__timep__ returns true if _object_ is of type __time__ and nil otherwise.`,
+			Examples: []string{
+				`(timep @2022-07-10T17:29:21.123456789Z) => t`,
+			},
+		}, &GiPkg)
+}
+
+// Timep represents the timep function.
+type Timep struct {
+	slip.Function
+}
+
+// Call the function with the arguments provided.
+func (f *Timep) Call(s *slip.Scope, args slip.List, depth int) slip.Object {
+	if len(args) != 1 {
+		slip.PanicArgCount(f, 1, 1)
+	}
+	if _, ok := args[0].(slip.Time); ok {
+		return slip.True
+	}
+	return nil
+}
