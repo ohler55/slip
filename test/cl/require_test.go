1d6780a8d3f6096fdfc46739632e48a6c3d58552
Peter Ohler <peter@ohler.com>
2022-12-18 19:33:47 -0500

    Add require function

---
+++ test/cl/require_test.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2022, Peter Ohler, All rights reserved.
+
+package cl_test
+
+import (
+	"testing"
+
+	"github.com/ohler55/ojg/tt"
+	"github.com/ohler55/slip"
+	"github.com/ohler55/slip/sliptest"
+)
+
+func TestRequireLoadPath(t *testing.T) {
+	slip.CurrentPackage.Set("*package-load-path*", slip.String("testplugin"))
+	tf := sliptest.Function{
+		Source: `(require 'testplugin)`,
+		Expect: "",
+	}
+	tf.Test(t)
+	result := slip.CompileString("(plug)").Eval(slip.NewScope(), 0)
+	tt.Equal(t, `"Plugged it"`, slip.ObjectString(result))
+}
+
+func TestRequirePathArg(t *testing.T) {
+	slip.CurrentPackage.Set("*package-load-path*", nil)
+	(&sliptest.Function{
+		Source: `(require "testplugin" "no-a-good-path")`,
+		Panics: true,
+	}).Test(t)
+}
+
+func TestRequireArgCount(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(require 'testplugin "no-a-good-path" t)`,
+		Panics: true,
+	}).Test(t)
+}
+
+func TestRequireBadName(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(require t)`,
+		Panics: true,
+	}).Test(t)
+}
+
+func TestRequireBadPath(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(require "testplugin" '("testplugin" t))`,
+		Panics: true,
+	}).Test(t)
+	(&sliptest.Function{
+		Source: `(require "testplugin" t)`,
+		Panics: true,
+	}).Test(t)
+}
+
+func TestRequireBadLoadPath(t *testing.T) {
+	slip.CurrentPackage.Set("*package-load-path*", slip.List{slip.True, slip.String("testplugin")})
+	(&sliptest.Function{
+		Source: `(require "testplugin")`,
+		Panics: true,
+	}).Test(t)
+	slip.CurrentPackage.Set("*package-load-path*", slip.True)
+	(&sliptest.Function{
+		Source: `(require "testplugin")`,
+		Panics: true,
+	}).Test(t)
+}
+
+func TestRequireBadPkg(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(require "bad" "testplugin")`,
+		Panics: true,
+	}).Test(t)
+}
