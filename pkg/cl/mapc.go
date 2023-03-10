5bba11ce448ecd198eff4c9a986ab03aea98bcd0
Peter Ohler <peter@ohler.com>
2023-03-09 19:56:38 -0500

    Add mapc

---
+++ pkg/cl/mapc.go
@@ -0,0 +1,86 @@
+// Copyright (c) 2023, Peter Ohler, All rights reserved.
+
+package cl
+
+import (
+	"github.com/ohler55/slip"
+)
+
+func init() {
+	slip.Define(
+		func(args slip.List) slip.Object {
+			f := Mapc{Function: slip.Function{Name: "mapc", Args: args}}
+			f.Self = &f
+			return &f
+		},
+		&slip.FuncDoc{
+			Name: "mapc",
+			Args: []*slip.DocArg{
+				{
+					Name: "function",
+					Type: "symbol|lambda",
+					Text: "The function to call for each entry in _hash-table_.",
+				},
+				{Name: "&rest"},
+				{
+					Name: "lists",
+					Type: "list",
+					Text: "The lists to iterate over.",
+				},
+			},
+			Return: "nil",
+			Text: `__mapc__ calls _function_ for each entry in the _lists_
+an argument from each list.`,
+			Examples: []string{
+				"(setq list '())",
+				"(mapc (lambda (x) (setq list (cons x list))) '(1 2 3)) => (1 2 3)",
+				"list => (3 2 1)",
+			},
+		}, &slip.CLPkg)
+}
+
+// Mapc represents the mapc function.
+type Mapc struct {
+	slip.Function
+}
+
+// Call the function with the arguments provided.
+func (f *Mapc) Call(s *slip.Scope, args slip.List, depth int) (result slip.Object) {
+	slip.ArgCountCheck(f, args, 2, -1)
+	pos := len(args) - 1
+	fn := args[pos]
+	d2 := depth + 1
+	caller := resolveToCaller(s, fn, d2)
+
+	pos--
+	list, ok := args[pos].(slip.List)
+	if !ok {
+		slip.PanicType("lists", args[pos], "list")
+	}
+	if 0 < pos {
+		min := len(list)
+		var l2 slip.List
+		for i := 0; i < pos; i++ {
+			if l2, ok = args[i].(slip.List); !ok {
+				slip.PanicType("lists", args[i], "list")
+			}
+			if len(l2) < min {
+				min = len(l2)
+			}
+		}
+		ca := make(slip.List, pos+1)
+		for n := 1; n <= min; n++ {
+			for i := 0; i <= pos; i++ {
+				l2 := args[i].(slip.List)
+				ca[i] = l2[len(l2)-n]
+			}
+			_ = caller.Call(s, ca, d2)
+		}
+	} else {
+		// The most common case.
+		for i := len(list) - 1; 0 <= i; i-- {
+			_ = caller.Call(s, slip.List{list[i]}, d2)
+		}
+	}
+	return list
+}
