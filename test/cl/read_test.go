8258499361299e0758b5c6401357ad612e46be5e
Peter Ohler <peter@ohler.com>
2024-02-03 20:12:53 -0500

    Add read function

---
+++ test/cl/read_test.go
@@ -0,0 +1,69 @@
+// Copyright (c) 2024, Peter Ohler, All rights reserved.
+
+package cl_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ohler55/slip"
+	"github.com/ohler55/slip/sliptest"
+)
+
+type badReader int
+
+func (w badReader) Read([]byte) (int, error) {
+	return 0, fmt.Errorf("oops")
+}
+
+func TestReadStream(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(read (make-string-input-stream " 123 "))`,
+		Expect: `123`,
+	}).Test(t)
+}
+
+func TestReadEOF(t *testing.T) {
+	(&sliptest.Function{
+		Source:    `(read (make-string-input-stream "(123 "))`,
+		PanicType: slip.Symbol("parse-error"),
+	}).Test(t)
+}
+
+func TestReadEOFValue(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(read (make-string-input-stream "(123 ") nil 'eof-error)`,
+		Expect: `eof-error`,
+	}).Test(t)
+}
+
+func TestReadEmptyEOFValue(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(read (make-string-input-stream "  ") nil 'eof-error)`,
+		Expect: `eof-error`,
+	}).Test(t)
+}
+
+func TestReadNotStream(t *testing.T) {
+	(&sliptest.Function{
+		Source:    `(read "123")`,
+		PanicType: slip.Symbol("type-error"),
+	}).Test(t)
+}
+
+func TestReadEmpty(t *testing.T) {
+	(&sliptest.Function{
+		Source:    `(read (make-string-input-stream "  "))`,
+		PanicType: slip.Symbol("error"),
+	}).Test(t)
+}
+
+func TestReadError(t *testing.T) {
+	scope := slip.NewScope()
+	scope.Let(slip.Symbol("in"), &slip.InputStream{Reader: badReader(0)})
+	(&sliptest.Function{
+		Scope:     scope,
+		Source:    `(read in)`,
+		PanicType: slip.Symbol("error"),
+	}).Test(t)
+}
