02325db547e0627b0148e8e74e9975dc89128b20
Peter Ohler <peter@ohler.com>
2024-04-25 18:53:33 -0400

    WIP

---
+++ test/cl/array-in-bounds-p_test.go
@@ -0,0 +1,54 @@
+// Copyright (c) 2024, Peter Ohler, All rights reserved.
+
+package cl_test
+
+import (
+	"testing"
+
+	"github.com/ohler55/slip"
+	"github.com/ohler55/slip/sliptest"
+)
+
+func TestArrayInBoundsPVector(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(array-in-bounds-p (make-array 4) 2)`,
+		Expect: "t",
+	}).Test(t)
+	(&sliptest.Function{
+		Source: `(array-in-bounds-p (make-array 4) 5)`,
+		Expect: "nil",
+	}).Test(t)
+}
+
+func TestArrayInBoundsPArray(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(array-in-bounds-p (make-array '(2 3)) 1 2)`,
+		Expect: "t",
+	}).Test(t)
+	(&sliptest.Function{
+		Source: `(array-in-bounds-p (make-array '(2 3)) 1 3)`,
+		Expect: "nil",
+	}).Test(t)
+}
+
+func TestArrayInBoundsPNotArray(t *testing.T) {
+	(&sliptest.Function{
+		Source:    `(array-in-bounds-p t)`,
+		PanicType: slip.TypeErrorSymbol,
+	}).Test(t)
+}
+
+func TestArrayInBoundsPBadDims(t *testing.T) {
+	(&sliptest.Function{
+		Source:    `(array-in-bounds-p (make-array '(2 3)) 1)`,
+		PanicType: slip.ErrorSymbol,
+	}).Test(t)
+	(&sliptest.Function{
+		Source:    `(array-in-bounds-p (make-array '(2 3)) 1 2 3)`,
+		PanicType: slip.ErrorSymbol,
+	}).Test(t)
+	(&sliptest.Function{
+		Source:    `(array-in-bounds-p (make-array '(2 3)) 1 t)`,
+		PanicType: slip.TypeErrorSymbol,
+	}).Test(t)
+}
