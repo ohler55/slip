b58dff5cade9e7e79d41849de9e786121b4490c3
Peter Ohler <peter@ohler.com>
2025-01-23 20:55:15 -0500

    Files and dirs (#105)
    * WIP
    
    * Add file-info function
    
    * Add glob function

---
+++ test/gi/glob_test.go
@@ -0,0 +1,33 @@
+// Copyright (c) 2025, Peter Ohler, All rights reserved.
+
+package gi_test
+
+import (
+	"testing"
+
+	"github.com/ohler55/slip"
+	"github.com/ohler55/slip/sliptest"
+)
+
+func TestGlobList(t *testing.T) {
+	(&sliptest.Function{
+		Source: `(let (matches)
+                  (glob (lambda (x) (addf matches x)) "testdata/*.lisp")
+                  matches)`,
+		Expect: `("testdata/comp.lisp" "testdata/sys-test.lisp")`,
+	}).Test(t)
+}
+
+func TestGlobNotString(t *testing.T) {
+	(&sliptest.Function{
+		Source:    `(glob (lambda (x) (addf result x)) t)`,
+		PanicType: slip.TypeErrorSymbol,
+	}).Test(t)
+}
+
+func TestGlobBadPattern(t *testing.T) {
+	(&sliptest.Function{
+		Source:    `(glob (lambda (x) (addf result x)) "][.lisp")`,
+		PanicType: slip.ErrorSymbol,
+	}).Test(t)
+}
