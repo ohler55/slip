abad8cd2c6245b819bfbded829409b312a73c1d6
nithronium <osmankuzucu5@gmail.com>
2026-01-23 20:12:05 +0300

    fix: addition of panic on file-author for windows builds

--- pkg/cl/file-author-windows.go
+++ pkg/cl/file-author-windows.go
@@ -7,5 +7,5 @@ import "errors"
 func getFileOwner(path string) (string, error) {
 	// On Windows, file-author is not supported and should raise a file-error
 	// according to the Common Lisp HyperSpec.
-	return "", errors.New("file-author is not supported on Windows")
+	panic(slip.FileErrorNew(slip.NewScope(), 0, slip.String(path), "file-author is not supported on Windows"))
 }
